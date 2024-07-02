94b11e188bd3299cd66f90491e2948964e960f66
Gustavo Minatto <gustavominatto23@gmail.com>
2024-07-02 14:52:18 -0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -5,12 +5,9 @@ import Controls from "./controls.js"
 const router = new Router()
 const controls = Controls()
 
-
-
 router.add("/", "/pages/home.html")
 router.add("/universe", "/pages/universe.html")
 router.add("/exploracao", "/pages/exploracao.html")
-
 
 router.handle()
 
